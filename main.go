973d9d8b545e206c2b84e8143a69f0cf23550022
Harsh97x <Rynzie@protonmail.com>
2023-07-22 16:02:07 +0530

    added chi, cors, vendor, router, handler and port number

--- main.go
+++ main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello there!")
+	godotenv.Load(".env")
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT is not found in the environment")
+	}
+	fmt.Println("PORT:", portString)
+
+	router := chi.NewRouter()
+
+	router.Use(
+		cors.Handler(
+			cors.Options{
+				AllowedOrigins: []string{"https://*", "http://*"},
+				AllowedMethods: []string{
+					"GET", "POST", "PUT", "DELETE", "OPTIONS",
+				},
+				AllowedHeaders:   []string{"*"},
+				ExposedHeaders:   []string{"Link"},
+				AllowCredentials: false,
+				MaxAge:           300,
+			},
+		),
+	)
+
+	srv := &http.Server{Handler: router, Addr: ":" + portString}
+	log.Printf("Server is starting at Port: %v", portString)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
