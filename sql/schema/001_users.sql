0bbc22b18e3fbc9f01a9948ce9b67bfdda0ff0e5
Harsh97x <Rynzie@protonmail.com>
2023-07-25 23:00:06 +0530

    generated database using sqlc

--- sql/schema/001_users.sql
+++ sql/schema/001_users.sql
@@ -1,9 +1,9 @@
--- +goose up
-
+-- +goose Up
 CREATE TABLE users (
-id UUID PRIMARY KEY
-create_at TIMESTAMP NOT NULL
-update_at TIMESTAMP NOT NULL
-name TEXT NOT NULL
+  id UUID PRIMARY KEY,
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP NOT NULL,
+  name TEXT NOT NULL
 );
--- +goose down
+-- +goose Down
+DROP TABLE users;
